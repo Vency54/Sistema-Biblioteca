2ec412c2830e91c1a28643aaa304fead62139bd1
Vency54 <jv028venceslau@gmail.com>
2026-09-18 11:46:39 -0300

    atualiza o gráfico

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { getEmprestimo } from "@/type/Emprestimo";
 import Grafico from "@/components/graficoSituacao";
 import Contador from "@/components/ContadorTabelas";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const livros = await getLivros();
   const leitores = await getLeitor();
